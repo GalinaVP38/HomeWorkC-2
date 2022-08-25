b2980ae7553321d7718a3992b80958da2026215a
Galina <pgv21@yandex.ru>
2022-08-25 15:06:44 +0300

    saveTask1

---
+++ Seminar2_Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+
+int num1 = num/100;
+int num2 = num - num1*100;
+int num3 = num2/10;
+
+Console.WriteLine(num3);
